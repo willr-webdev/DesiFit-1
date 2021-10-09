ac073fabcb65a1e2dfc3b05534878909ade8d579
willr-webdev <81498360+willr-webdev@users.noreply.github.com>
2021-10-09 01:54:08 -0400

    u

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-function toggleNav() {
+function openMobileNav() {
+  document.querySelector(".fa-bars").style.display = "none";
+}
 
+function closeMobileNav() {
+  document.querySelector(".fa-window-close").style.display = "none";
 }
