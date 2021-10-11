32b855a2d100c5133deb8d586cc7e68596e55dea
willr-webdev <81498360+willr-webdev@users.noreply.github.com>
2021-10-11 18:38:58 -0400

    u

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-function openMobileNav() {
-  document.querySelector(".fa-bars").style.display = "none";
-  document.querySelector(".open-nav").style.display = "flex";
-}
+// function openMobileNav() {
+//   document.querySelector(".fa-bars").style.display = "none";
+//   document.querySelector(".open-nav").style.display = "flex";
+// }
 
-function closeMobileNav() {
-  document.querySelector(".open-nav").style.display = "none";
-  document.querySelector(".fa-bars").style.display = "flex";
+// function closeMobileNav() {
+//   document.querySelector(".open-nav").style.display = "none";
+//   document.querySelector(".fa-bars").style.display = "flex";
+// }
+
+function opacity() {
+  if (checkBox.checked == true) {
+    document.querySelector(".main-wrapper").style.opacity = "0.25";
+  } else { return }
 }
